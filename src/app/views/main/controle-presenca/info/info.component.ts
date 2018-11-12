c62283fa8f9ed00e1716bea79815b406e8290e5c
GuilhermeHenrique_SMN <guilherme.henrique@smn.com.br>
2018-11-11 22:34:16 -0200

    Atualizações controle de presença

--- src/app/views/main/controle-presenca/info/info.component.ts
+++ src/app/views/main/controle-presenca/info/info.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService} from '../../../../core/api/api.service';
-import {UiToolbarService} from 'ng-smn-ui';
+import {UiElement, UiSnackbar, UiToolbarService} from 'ng-smn-ui';
 
 @Component({
     selector: 'app-controle-presenca-info',
@@ -11,21 +11,27 @@ import {UiToolbarService} from 'ng-smn-ui';
 export class ControlePresencaInfoComponent implements OnInit, AfterViewInit, OnDestroy {
     addingNew: boolean;
     info: any;
-    turmas: any[];
-    disciplinas: any[];
+    turmas: any;
+    disciplinas: any;
     status: any[];
+    quantidadePresencas: any[];
+    loading: boolean;
+    @ViewChild('formControle') formControle;
 
     constructor(
         public router: Router,
         private activedRoute: ActivatedRoute,
         private changeDetectorRef: ChangeDetectorRef,
         private api: ApiService,
-        private toolbarService: UiToolbarService
+        private toolbarService: UiToolbarService,
+        private element: ElementRef
     ) {
         this.info = {};
         this.turmas = [];
         this.disciplinas = [];
         this.status = [];
+
+        this.quantidadePresencas = [{qtd: 2}, {qtd: 4}];
     }
 
     ngOnInit() {
@@ -53,61 +59,88 @@ export class ControlePresencaInfoComponent implements OnInit, AfterViewInit, OnD
     }
 
     getInfo() {
+        this.api
+            .prep('administracao', 'controlePresenca', 'selecionarPorId')
+            .call({id: this.activedRoute.snapshot.params['id']})
+            .subscribe(res => {
+                this.info = res.content;
+            }, err => {
+                UiSnackbar.show({
+                    text: err.message
+                });
+            })
     }
 
+
     onSubmit() {
+        if(!this.loading) {
+            for (const control in this.formControle.controls) {
+                if (this.formControle.controls.hasOwnProperty(control)) {
+                    this.formControle.controls[control].markAsTouched();
+                    this.formControle.controls[control].markAsDirty();
+                }
+            }
+
+            if(this.formControle.invalid) {
+                UiElement.focus(this.element.nativeElement.querySelector('form .ng-invalid'));
+                return false;
+            }
+
+            this.loading = true;
+            if(this.addingNew) {
+                this.api
+                    .prep('administracao', 'controlePresenca', 'inserir')
+                    .call(this.info)
+                    .subscribe((res) => {
+                        UiSnackbar.show({
+                            text: res.content.message
+                        });
+
+                        this.router.navigate(['/controle-presenca/', res.content.id]);
+                    }, (err) => {
+                        UiSnackbar.show({
+                            text: err.message
+                        });
+                    }, () => {
+                        this.loading = false;
+                    });
+            }
+        }
     }
 
     getDisciplina() {
-        /*this.api.prep(
-            'administracao',
-            'disciplina',
-            'selecionar'
-        ).call({idTurma: this.info.idTurma})
-            .subscribe(res => {
-                    this.disciplinas = res.content;
-                }, null,
-                () => {
+        if (!this.disciplinas.loading) {
+            this.disciplinas.loading = true;
 
-                }
-            );*/
-
-        this.disciplinas = [
-            {id: 1234, nome: 'Produção de Bugs'},
-            {id: 4567, nome: 'Filosofia com Piton'},
-            {id: 8910, nome: 'Batata'}
-        ];
+            this.api
+                .prep(
+                    'administracao',
+                    'disciplina',
+                    'selecionar'
+                ).call({idTurma: this.info.idTurma})
+                .subscribe(res => {
+                        this.disciplinas = res.content;
+                        if(this.disciplinas.length == 1) {
+                            this.info.idDisciplina = this.disciplinas[0].id;
+                            this.info.nomeDisciplina = this.disciplinas[0].nome;
+                        }
+                    }, null,
+                    () => {
+                        this.disciplinas.loading = false;
+                    }
+                );
+        }
     }
 
     getTurma() {
-        /* this.api.prep(
-             'administracao',
-             'turma',
-             'selecionar'
-         ).call()
-             .subscribe(res => {
-                     this.turmas = res.content;
-                 }, null,
-                 () => {
-
-                 }
-             );*/
-
-        this.turmas = [
-            {id: 237, nome: '4º SEM Sistemas de Informação', semestre: 4},
-            {id: 310, nome: '4º SEM Engenharia de Software', semestre: 4},
-            {id: 123, nome: '2º SEM Batata', semestre: 2}
-        ];
-    }
-
-    getStatus() {
-        this.api.prep(
-            'administracao',
-            'status',
-            'selecionarSimples'
-        ).call()
+        this.api
+            .prep(
+                'administracao',
+                'turma',
+                'selecionar'
+            ).call()
             .subscribe(res => {
-                    this.status = res.content;
+                    this.turmas = res.content;
                 }, null,
                 () => {
 
@@ -115,13 +148,55 @@ export class ControlePresencaInfoComponent implements OnInit, AfterViewInit, OnD
             );
     }
 
+    getStatus() {
+        this.api
+            .prep(
+                'administracao',
+                'status',
+                'selecionarSimples'
+            ).call()
+            .subscribe(res => {
+                    this.status = res.content;
+                }
+            );
+    }
+
     getSemestre() {
         this.turmas.forEach(item => {
-            if(item.id === this.info.idTurma) {
-                this.info.semestre = item.semestre;
+            if (item.id == this.info.idTurma) {
+                this.info.semestre = +item.semestre;
+                this.info.nomeTurma = item.nome;
             }
         });
 
-        this.getDisciplina();
+        if(this.addingNew) {
+            this.info.idDisciplina = null;
+            this.getDisciplina();
+        }
+
+    }
+
+    getNomeDisciplina() {
+        this.disciplinas.forEach(item => {
+            if(item.id == this.info.idDisciplina) {
+                this.info.nomeDisciplina = item.nome;
+            }
+        });
+    }
+
+    cancelarControle() {
+        this.api
+            .prep('administracao', 'controlePresenca', 'cancelar')
+            .call({id: this.info.id})
+            .subscribe(res => {
+                UiSnackbar.show({
+                    text: res.message
+                });
+                this.router.navigate(['/controle-presenca']);
+            }, err => {
+                UiSnackbar.show({
+                    text: err.message
+                });
+            })
     }
 }
