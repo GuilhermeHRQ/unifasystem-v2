4f032039b76d248dc52498060501869dea0ee237
Miguel Aleixo <miguelgomes2099@hotmail.com>
2018-11-13 22:06:41 -0200

    inserindo da presença

--- src/app/views/aluno-presenca/presenca/presenca.component.ts
+++ src/app/views/aluno-presenca/presenca/presenca.component.ts
@@ -1,4 +1,7 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ElementRef } from '@angular/core';
+import {UiElement} from 'ng-smn-ui';
+import { ApiService } from '../../../core/api/api.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-aluno-presenca',
@@ -6,8 +9,38 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
     styleUrls: ['./presenca.component.scss']
 })
 export class PresencaComponent implements OnInit, AfterViewInit {
+    codigo: number;
 
-    constructor() {
+    constructor(private element: ElementRef,
+                private api: ApiService) {
+    }
+
+    onSubmit(form) {
+        for (const control in form.controls) {
+            if (form.controls.hasOwnProperty(control)) {
+                form.controls[control].markAsTouched();
+                form.controls[control].markAsDirty();
+                console.log('entrou')
+            }
+        }
+        if (!form.valid) {
+            UiElement.focus(this.element.nativeElement.querySelector('form .ng-invalid'));
+            return false;
+        }
+
+        this.api
+            .http('POST', `${environment.AUTH_API}/aluno-presenca/${this.codigo}`)
+            .call()
+            .subscribe( res => {
+                console.log(res.content)
+            }, err => {
+                console.log(err)
+                UiSnackBar.show()
+            }, () => {
+
+            })
+        
+
     }
 
     ngOnInit() {
